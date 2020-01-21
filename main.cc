aa8d2183b43b0af68a5dcf6b9eb1ab56d7d5657f
rodsan0 <rodsan0@users.noreply.github.com>
2020-01-21 13:40:51 -0500

    fix BitMarking declaration; put things into MatchBin

--- main.cc
+++ main.cc
@@ -5,15 +5,26 @@
 #include "tools/matchbin_utils.h"
 
 int main() {
+    // initialize MatchBin
 
     emp::Random random(1);
-
     emp::MatchBin<
-        int;
+        int,
         emp::NextUpMetric<>,
-        emp::RandomSelector<>,
-        emp::NopRegulator<>
+        emp::RankedSelector<>,
+        emp::NopRegulator
     > bin(random);
 
-    
+    // put 10 things into MatchBin
+    bin.Put(0, 0);
+    bin.Put(1, 23);
+    bin.Put(2, 44);
+    bin.Put(3, 987);
+    bin.Put(4, 990);
+    bin.Put(5, 3423);
+    bin.Put(6, 9878);
+    bin.Put(7, 2213);
+    bin.Put(8, 2);
+    bin.Put(9, 653);
+
 }

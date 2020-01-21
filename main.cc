26e562e3752fc1eec8f31f59fe0cd9c6dc1ec987
rodsan0 <rodsan0@users.noreply.github.com>
2020-01-21 14:18:21 -0500

    implement command line interactiveness

--- main.cc
+++ main.cc
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <chrono>
-
+#include <cstdlib>
+#include <stdlib.h>
+	
+#include "tools/Random.h"
 #include "tools/MatchBin.h"
 #include "tools/matchbin_utils.h"
 
-size_t ITERATIONS = 10000;
-
-int main() {
+int main(int argc, char *argv[]) {
     // initialize MatchBin
+    size_t iterations;
+    size_t seed; 
 
-    emp::Random random(1);
+    if (argc != 3) {
+        // defaults
+        iterations = 1000;
+        seed = 2;
+    } else {
+        iterations = std::atoi(argv[1]);
+        seed = std::atoi(argv[2]);
+    }
+
+    emp::Random random(seed);
+
     emp::MatchBin<
         int,
         emp::NextUpMetric<>,
@@ -17,25 +30,27 @@ int main() {
         emp::NopRegulator
     > bin(random);
 
-    // put 10 things into MatchBin
-    bin.Put(0, 0);
-    bin.Put(1, 23);
-    bin.Put(2, 44);
-    bin.Put(3, 987);
-    bin.Put(4, 990);
-    bin.Put(5, 3423);
-    bin.Put(6, 9878);
-    bin.Put(7, 2213);
-    bin.Put(8, 2);
-    bin.Put(9, 653);
+    // seed rand()
+    std::srand(seed);
 
-    /* benchmark! */
+    // put elements into MatchBin
+
+    std::cout << "Putting elements into MatchBin..." << std::endl;
+
+    for (size_t tag = 0; tag < iterations; tag++) {
+        int value = std::rand();
+        bin.Put(value, tag);
+    }
+
+    /* ------ BENCHMARK! ------ */
     size_t result = 0;
+
+    std::cout << "Benchmarking..." << std::endl;
 
     // start hig-res timer
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    for (size_t i; i < ITERATIONS; i++) {
+    for (size_t i = 0; i < iterations; i++) {
         // get values
         emp::vector<int> matches = bin.GetVals(bin.Match(i));
 
@@ -46,12 +61,13 @@ int main() {
     // end timer
     auto end_time = std::chrono::high_resolution_clock::now();
 
-    // print final result, just in case the compiler feels like optimizing it away
-    std::cout << result << std::endl;
+    std::cout << "Results!\n" << std::endl;
 
+    // print final result, just in case the compiler feels like optimizing it away
+    std::cout << "result: " << result << std::endl;
 
     // calculate and print timing
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
-    std::cout << duration << std::endl;
+    std::cout << "duration: " << duration << std::endl;
 
 }
